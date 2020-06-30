e99513580d8b46e7e3a0577512a7eb2ab0b302c5
aland1013 <adam.land@gmail.com>
2020-06-29 20:49:59 -0600

    through 49

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,7 @@ import { ThemeProvider } from '@material-ui/styles';
 
 import theme from './ui/Theme';
 import Header from '../components/ui/Header';
+import Footer from '../components/ui/Footer';
 
 function App() {
   return (
@@ -11,7 +12,11 @@ function App() {
       <BrowserRouter>
         <Header />
         <Switch>
-          <Route exact path='/' component={() => <div>Home</div>} />
+          <Route
+            exact
+            path='/'
+            component={() => <div style={{ height: '2000px' }}>Home</div>}
+          />
           <Route exact path='/services' component={() => <div>Services</div>} />
           <Route
             exact
@@ -37,6 +42,7 @@ function App() {
           />
           <Route exact path='/estimate' component={() => <div>Estimate</div>} />
         </Switch>
+        <Footer />
       </BrowserRouter>
     </ThemeProvider>
   );
